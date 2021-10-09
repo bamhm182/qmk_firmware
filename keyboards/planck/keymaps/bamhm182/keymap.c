72b8935de92645e73191c49e1843f5413265def2
bamhm182 <bamhm182@gmail.com>
2021-10-09 18:45:06 -0500

    Configured layer LEDs

--- keyboards/planck/keymaps/bamhm182/keymap.c
+++ keyboards/planck/keymaps/bamhm182/keymap.c
@@ -16,7 +16,8 @@
 
 #include QMK_KEYBOARD_H
 #include "muse.h"
-
+#include "eeprom.h"
+#include "print.h"
 
 enum planck_layers {
   _COLEMAK,
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * 2 | Del   |        | MS_L   | MS_D   | MS_R   |        |        | MS_LC  | MS_RC  |   {    |   }    |  |     |
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * 3 |       |        |        |        |        |        |        |        |        | Home   | End    |        |
+ * 3 |       |        |        |        |        |        |        | PageDn | PageUp | Home   | End    |        |
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * 4 |       |        |        |        |        |                 |        | Next   | Vol-   | Vol+   | Play   |
  *   `----------------------------------------------------------------------------------------------------------'
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
       KC_TILD, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______,
       KC_DEL , _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_BTN1, KC_BTN2, KC_LCBR, KC_RCBR, KC_PIPE,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,
+      _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_HOME, KC_END,  _______,
       _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -102,22 +103,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * 3 |       |        |        |        |        |        |        |   0    |   1    |   2    |   3    |        |
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * 4 |       |        |        |        |        |                 |        | Next   | Vol-   | Vol+   | Play   |
+ * 4 |       |        |        |        |        |                 |        |        |        |        |        |
  *   `----------------------------------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
       KC_GRV , _______, _______, _______, _______, _______, _______, KC_PLUS, KC_7   , KC_8   , KC_9   , KC_ASTR,
       KC_DEL , _______, _______, _______, _______, _______, _______, KC_MINS, KC_4   , KC_5   , KC_6   , KC_SLSH,
       _______, _______, _______, _______, _______, _______, _______, KC_0   , KC_1   , KC_2   , KC_3   , _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
  *       1       2        3        4        5        6        7        8        9        10       11       12
  *   ,----------------------------------------------------------------------------------------------------------.
- * 1 |AUtog  |MIDtog  |MUStog  |MUSmod  |RGBtog  |RGBmod  |        |        |        |        |        |        |
+ * 1 | AUtog | MIDtog | MUStog | MUSmod | RGBtog | RGBmod | Debug  |        |        |        |        |        |
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * 2 | F1    | F2     | F3     | F4     | F5     | F6     |        |        |        |        |        | Reset  |
+ * 2 | F1    | F2     | F3     | F4     | F5     | F6     |        | Vol-   | Vol+   | Skip   | Play   | Reset  |
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * 3 | F7    | F8     | F9     | F10    | F11    | F12    |        |        |        |        |        |        |
  *   |-------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -125,13 +126,88 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   `----------------------------------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-      AU_TOG , MI_TOG , MU_TOG , MU_MOD , RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______,
-      KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , _______, _______, _______, _______, _______, RESET  ,
+      AU_TOG , MI_TOG , MU_TOG , MU_MOD , RGB_TOG, RGB_MOD, DEBUG  , _______, _______, _______, _______, _______,
+      KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , _______, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, RESET  ,
       KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
+
+
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void) {
+  rgb_matrix_enable();
+}
+
+#define CLR_NON {  0,  0,  0}
+#define CLR_RED {243,222,234}
+#define CLR_PUR {169,120,255}
+#define CLR_GRN { 85,203,158}
+#define CLR_BLU {134,255,213}
+#define CLR_ORG { 10,225,255}
+#define CLR_YLW { 32,176,255}
+
+const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
+    /* Lower */
+    [1] = {
+      CLR_RED, CLR_NON, CLR_GRN, CLR_BLU, CLR_GRN, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_BLU, CLR_BLU, CLR_NON,
+      CLR_RED, CLR_NON, CLR_BLU, CLR_BLU, CLR_BLU, CLR_NON, CLR_NON, CLR_GRN, CLR_GRN, CLR_BLU, CLR_BLU, CLR_BLU,
+      CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_ORG, CLR_ORG, CLR_GRN, CLR_GRN, CLR_NON,
+      CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON         , CLR_NON, CLR_YLW, CLR_YLW, CLR_YLW, CLR_YLW
+    },
+    /* Raise */
+    [2] = {
+      CLR_RED, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_PUR, CLR_BLU, CLR_BLU, CLR_BLU, CLR_PUR,
+      CLR_RED, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_PUR, CLR_BLU, CLR_BLU, CLR_BLU, CLR_PUR,
+      CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_NON,
+      CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON         , CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON
+    },
+    /* Adjust */
+    [3] = {
+      CLR_GRN, CLR_GRN, CLR_YLW, CLR_YLW, CLR_RED, CLR_RED, CLR_ORG, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON,
+      CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_NON, CLR_PUR, CLR_PUR, CLR_PUR, CLR_PUR, CLR_ORG,
+      CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_BLU, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON,
+      CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON         , CLR_NON, CLR_NON, CLR_NON, CLR_NON, CLR_NON
+    }
+};
+
+void set_layer_color(int layer) {
+  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+    HSV hsv = {
+      .h = pgm_read_byte(&ledmap[layer][i][0]),
+      .s = pgm_read_byte(&ledmap[layer][i][1]),
+      .v = pgm_read_byte(&ledmap[layer][i][2]),
+    };
+    if (!hsv.h && !hsv.s && !hsv.v) {
+        rgb_matrix_set_color( i, 0, 0, 0 );
+    } else {
+        RGB rgb = hsv_to_rgb( hsv );
+        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    }
+  }
+}
+
+void rgb_matrix_indicators_user(void) {
+  if (keyboard_config.disable_layer_led) { return; }
+  switch (biton32(layer_state)) {
+    case 1:
+      set_layer_color(1);
+      break;
+    case 2:
+      set_layer_color(2);
+      break;
+    case 3:
+      set_layer_color(3);
+      break;
+   default:
+    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+      rgb_matrix_set_color_all(0, 0, 0);
+    break;
+  }
+}
 
 #ifdef AUDIO_ENABLE
   float plover_song[][2]     = SONG(PLOVER_SOUND);
@@ -144,12 +220,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
     case BACKLIT:
       if (record->event.pressed) {
         register_code(KC_RSFT);
